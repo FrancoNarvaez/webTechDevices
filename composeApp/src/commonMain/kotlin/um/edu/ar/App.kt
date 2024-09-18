0644c5ad8d32cbe085479a75406e00305d7ddf2c
FrancoNarvaez <f.narvaez@alumno.um.edu.ar>
2024-09-17 23:33:09 -0300

    initial commit

---
+++ composeApp/src/commonMain/kotlin/um/edu/ar/App.kt
@@ -0,0 +1,24 @@
+package um.edu.ar
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import um.edu.ar.ui.login.ui.LoginScreen
+
+import webtechdevices.composeapp.generated.resources.Res
+import webtechdevices.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    LoginScreen()
+}
---
+++ composeApp/src/commonMain/kotlin/um/edu/ar/ui/login/ui/LoginScreen.kt
@@ -0,0 +1,8 @@
+package um.edu.ar.ui.login.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+
+}
