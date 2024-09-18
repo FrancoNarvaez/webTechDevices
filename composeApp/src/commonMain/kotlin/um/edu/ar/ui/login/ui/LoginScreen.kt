d02d1edfdee57c55d1a09072eb15030deed0e323
FrancoNarvaez <f.narvaez@alumno.um.edu.ar>
2024-09-17 23:42:41 -0300

    login

--- composeApp/src/commonMain/kotlin/um/edu/ar/ui/login/ui/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/um/edu/ar/ui/login/ui/LoginScreen.kt
@@ -1,8 +1,24 @@
 package um.edu.ar.ui.login.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
 
 @Composable
 fun LoginScreen() {
+    Box(Modifier.fillMaxSize().padding(16.dp)) {
+        Login(Modifier.align(Alignment.Center))
+    }
+}
 
+@Composable
+fun Login(modifier: Modifier) {
+    Column(modifier = modifier) {
+
+    }
 }
